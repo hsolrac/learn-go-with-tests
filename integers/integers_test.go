601f0ae3ca0370585a26d372b48d1de6ee81a36e
Carlos Henrique <ch.henriquevinha@gmail.com>
2023-02-10 22:08:10 -0300

    chore: integers

---
+++ integers/integers_test.go
@@ -0,0 +1,14 @@
+package integers
+
+import (
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("Expected '%d' but go '%d'", expected, sum)
+	}
+}
